81ec5e8173e9c34cdd16b170440b5d582ccc8231
Neal Richardson <neal.p.richardson@gmail.com>
2019-09-17 17:57:00 -0700

    Off by one? [skip ci]

--- r/tests/testthat/test-Array.R
+++ r/tests/testthat/test-Array.R
@@ -399,11 +399,11 @@ test_that("array() does not convert doubles to integer", {
 })
 
 test_that("array() converts raw vectors to uint8 arrays (ARROW-3794)", {
+  skip("terminate called after throwing an instance of 'std::length_error'")
   expect_equal(array(as.raw(1:10))$type, uint8())
 })
 
 test_that("Array<int8>$as_vector() converts to integer (ARROW-3794)", {
-  skip("terminate called after throwing an instance of 'std::length_error'")
   a <- array((-128):127)$cast(int8())
   expect_equal(a$type, int8())
   expect_equal(a$as_vector(), (-128):127)
